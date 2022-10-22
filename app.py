e150dc46686804dfe844862691baae6d9f21632a
anacarolcortez <anacarolcortez@hotmail.com>
2022-10-22 17:46:32 -0300

    metodos com flask-restful exemplo

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 
 class Receita(Resource):
-    def get(self):
+    def get(self, id):
         return {'msg': 'Teste'}
 
-api.add_resource(Receita, '/')
+    def put(self, id):
+        return {'msg': 'Teste'}
+
+    def delete(self, id):
+        return {'msg': 'Teste'}
 
 
-
-
+api.add_resource(Receita, '/receitas/<id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
